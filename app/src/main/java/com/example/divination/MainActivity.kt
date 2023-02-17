c5ca16aba4c4c34f47fbb588e4a46588bc7d2bdb
jordanlat <jordanlat@hotmail.fr>
2023-02-17 16:02:55 +0100

    Ajouts de commentaires et déplacement de fonctions et de variables.

--- app/src/main/java/com/example/divination/MainActivity.kt
+++ app/src/main/java/com/example/divination/MainActivity.kt
@@ -7,67 +7,74 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.concurrent.schedule
 import kotlin.random.Random
-import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editText: EditText
+    //Déclaration de constantes
+    private val msgListe = ArrayList<String>()
+    private val layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+    private val recyclerAdapter = MsgAdapter(msgListe,this@MainActivity)
+    private var trigAddMsg = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Déclaration de constante lié aux Layout/visuel
+        val editText: EditText = findViewById((R.id.user_input))
+        val msgRecyclerView = findViewById<RecyclerView>(R.id.msg_recycler_view)
+
+
         // Pour cacher la bar qui contient le titre
         supportActionBar?.hide()
 
-        val msgListe = ArrayList<String>()
-        editText = findViewById((R.id.user_input))
+
+        // On force le focus sur l'entré utilisateur
         editText.requestFocus()
 
+        // À la validation par clavier, on prépare le message et on l'affiche
         editText.setOnKeyListener(object : View.OnKeyListener {
             @SuppressLint("ResourceType")
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
-
-                val thisText = editText.text
-                val layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
-                val recyclerAdapter = MsgAdapter(msgListe,this@MainActivity)
-                val msgRecyclerView = findViewById<RecyclerView>(R.id.msg_recycler_view)
-
+                var thisText = editText.text
                 layoutManager.stackFromEnd = true
 
-                if (event.keyCode == 66 && event.action == KeyEvent.ACTION_UP && thisText.isNotEmpty()) {
+                if (event.keyCode == 66 && event.action == KeyEvent.ACTION_DOWN && thisText.isNotEmpty()) {
 
+                    thisText = editText.text
                     msgListe.add(thisText.toString())
 
                     msgRecyclerView.layoutManager = layoutManager
                     msgRecyclerView.adapter = recyclerAdapter
 
-                    sendAnswer()
-
+                    val deferredResult = GlobalScope.async {
+                        //val asyngMsg = Async(Dispatchers.IO) {sendAnswer()}
+                        delay(2000)
+                        sendAnswer()
+                        //recyclerAdapter.notifyDataSetChanged()
+                    }
                     editText.text.clear()
                     return true
                 }
                 return false
             }
-
-            fun sendAnswer() {
-                val isEven = isEven(msgListe.size)
-                if(isEven){
-                    //Timer().schedule(1000){
-                        msgListe.add(generateAnswer())
-                    //}
-
-                }
+            // On enregistre une réponse auto-généré
+            suspend fun sendAnswer() {
+                delay(2000L)
+                msgListe.add(generateAnswer())
             }
         })
+        println("this is lifecycle: $lifecycle")
+        recyclerAdapter.notifyDataSetChanged()
     }
 
-
-    fun generateAnswer(): String {
+    // Renvoie une réponse généré
+    private fun generateAnswer(): String {
         val adverbLists = listOf(
             "Certainement",
             "Sûrement",
@@ -79,14 +86,14 @@ class MainActivity : AppCompatActivity() {
         val randomAdverb = adverbLists[Random.nextInt(adverbLists.size)]
         return "$randomAdverb Koha"
     }
-
-
 }
 
+//On met en forme les messages
 class MsgAdapter(private val msgList: ArrayList<String>, private val context: MainActivity) : RecyclerView.Adapter<MsgAdapter.MsgViewHolder>() {
 
+    @SuppressLint("RtlHardcoded")
     override fun onBindViewHolder(msgViewHolder: MsgViewHolder, index: Int) {
-        var gravityMsg = msgViewHolder.contentTextView.layoutParams as LinearLayout.LayoutParams
+        val gravityMsg = msgViewHolder.contentTextView.layoutParams as LinearLayout.LayoutParams
         if (!isEven(index)) {
             gravityMsg.gravity = Gravity.RIGHT
         } else {
@@ -97,7 +104,6 @@ class MsgAdapter(private val msgList: ArrayList<String>, private val context: Ma
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
-
         return MsgViewHolder(
             LayoutInflater.from(context).inflate(R.layout.message_question_1, parent, false)
         )
@@ -112,7 +118,7 @@ class MsgAdapter(private val msgList: ArrayList<String>, private val context: Ma
     }
 }
 
-
+// Détermine si un nombre est pair
 private fun isEven(number: Number): Boolean {
     return number.toInt() % 2 == 0
 }
